dc36ce7a35a0af87df95273b9a74eb18865c3851
Peter deHaan <peter@deseloper.com>
2023-03-24 11:04:37 -0700

    Add JSON stats

--- stats.js
+++ stats.js
@@ -12,19 +12,23 @@ const BYTES_OPTS = {
 
 const logos = await fs.readdir("logo_cache");
 
+const results = [];
 const errors = [];
 
 for (const logo of logos.filter((logo) => logo.endsWith(".png"))) {
   const logoPath = path.join(LOGO_DIR, logo);
   try {
-    const { width, height } = sizeOf(logoPath);
+    const res = sizeOf(logoPath);
+    const { width, height } = res;
     const { size } = await fs.stat(logoPath);
     console.log(
       `${logo} | ${width}x${height} | ${bytes(size, BYTES_OPTS)}`
     );
+    results.push({logo, ...res, size});
   } catch (err) {
     errors.push(`[${logoPath}] ${err.message}`);
   }
 }
 
 console.error("\n\n" + errors.join("\n"));
+await fs.writeFile("stats.json", JSON.stringify({results, errors}, null, 2));
