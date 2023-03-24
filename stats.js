638623f801e50dcc30e5a12b0f34eb62eea23c5b
Peter deHaan <peter@deseloper.com>
2023-03-24 11:26:00 -0700

    Better stats

--- stats.js
+++ stats.js
@@ -19,12 +19,22 @@ for (const logo of logos.filter((logo) => logo.endsWith(".png"))) {
   const logoPath = path.join(LOGO_DIR, logo);
   try {
     const res = sizeOf(logoPath);
-    const { width, height } = res;
+    if (res.images) {
+      const biggest = res.images.reduce((acc, curr) => {
+        if (curr.width * curr.height > acc.width * acc.height) {
+          return curr;
+        }
+        return acc;
+      });
+      res.width = biggest.width;
+      res.height = biggest.height;
+    }
+    let { width, height } = res;
     const { size } = await fs.stat(logoPath);
     console.log(
-      `${logo} | ${width}x${height} | ${bytes(size, BYTES_OPTS)}`
+      `${logo} | ${width}×${height} | ${res.type} | ${bytes(size, BYTES_OPTS)}`
     );
-    results.push({logo, ...res, size});
+    results.push({logo, ...res, dimensions: `${width}×${height}`, size});
   } catch (err) {
     errors.push(`[${logoPath}] ${err.message}`);
   }
